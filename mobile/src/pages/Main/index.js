f4e782f3025066fffee08046955aa2e1c070a64a
loxt <emannuelloxt@hotmail.com>
2020-01-27 14:54:02 -0400

    feat: header, menu, tabs and styles

--- mobile/src/pages/Main/index.js
+++ mobile/src/pages/Main/index.js
@@ -1,7 +1,45 @@
 import React from 'react';
 
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Title,
+  Description,
+  Annotation
+} from './styles';
+
+import Header from '../../components/Header';
+import Tabs from '../../components/Tabs';
+import Menu from '../../components/Menu';
 
 export default function Main() {
-  return <Container />;
+  return (
+    <Container>
+      <Header />
+
+      <Content>
+        <Menu />
+        <Card>
+          <CardHeader>
+            <Icon name='attach-money' size={28} color='#666' />
+            <Icon name='visibility-off' size={28} color='#666' />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 197.611,95</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>Transferência de R$ 20,00 recebida de Emannuel Matos hoje às 6:00h</Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
+      <Tabs />
+    </Container>
+  );
 }
